ef2fad469d33f4d7804479170216d1d89e628ea1
Canada-Broccoli <chen0969@algonquinlive.com>
2025-02-22 22:01:00 -0500

    add styles

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,11 @@ Route::get('/login', 'PageController@login');
 Route::get('/member/{account}', 'PageController@bcard');
 Route::get('/admin/member', 'PageController@member');
 
+// find password
+Route::get('/findpassword', function () {
+    return view('_login/findpassword');
+});
+
 Route::get('/api/get-role-info/{id}', function ($id) {
     $role = Role::find($id);
     if (!$role) {
